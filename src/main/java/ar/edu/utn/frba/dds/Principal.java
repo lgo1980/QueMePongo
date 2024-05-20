fea8a665b267af2142b62da6cc5aab5cedb37b32
Leonardo Olmedo <lgo80@yahoo.com.ar>
2024-05-20 00:36:54 -0300

    Estoy arrancando la 4 interaccion

--- src/main/java/ar/edu/utn/frba/dds/Principal.java
+++ src/main/java/ar/edu/utn/frba/dds/Principal.java
@@ -1,8 +1,13 @@
 package ar.edu.utn.frba.dds;
 
+import java.util.List;
+import java.util.Map;
+
 public class Principal {
 
   public static void main(String[] args) {
-
+    AccuWeatherAPI apiClima = new AccuWeatherAPI();
+    List<Map<String, Object>> condicionesClimaticas = apiClima.getWeather("Buenos Aires, Argentina");
+    System.out.println("Valor: " + condicionesClimaticas.get(0).get("PrecipitationProbability")); //Devuelve un número del 0 al 1 “”
   }
 }
